b998c8c3847aeb9b8674ef5aa6ee1cc79e483325
Kaeden Bryer <kaeden.bryer@tylertech.com>
2025-06-13 23:36:37 -0400

    added boilerplate angular code

---
+++ OUMCVB-website/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet, 
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'OUMCVB-website';
+}
